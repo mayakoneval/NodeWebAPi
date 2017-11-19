7a20b85b4182b333e6ada8a09f46e9429d489e51
mayakoneval <mayakoneval@gmail.com>
2017-11-19 18:51:33 -0500

    added end functionality

--- core/database.js
+++ core/database.js
@@ -13,6 +13,7 @@ pool.query("CREATE TABLE IF NOT EXISTS people(firstname varchar(80), lastname va
 var insert_text = "INSERT INTO people (firstname, lastname) values($1, $2) RETURNING *";
 
 function listTable (req, res) {
+  res.writeHead(200, {"Content-Type": "application/json"});
   pool.query('SELECT * FROM people')
       .then(response =>  {
         console.log(JSON.stringify(response.rows))
@@ -22,17 +23,26 @@ function listTable (req, res) {
       .catch(e => console.error(e.stack));
 }
 
-function addValue (req, res, name_array) {
+function addValue (req, res, query_string) {
+  res.writeHead(200, {"Content-Type": "application/json"});
+  var name_array = [query_string.firstname, query_string.lastname];
   pool.query(insert_text, name_array)
-    .then(res => {
-      console.log(res.rows[0]);
+    .then(response => {
+      res.write("Success ");
+      res.write(JSON.stringify(query_string));
+      console.log(response.rows[0]);
+      res.end();
     })
     .catch(e => console.error(e.stack));
 }
 
+function endPool() {
+  pool.end();
+}
+
 module.exports = {
   listTable: listTable, 
-  addValue: addValue
+  addValue: addValue,
+  endPool: endPool
 };
 
-//add a listener to end the pool
--- core/server.js
+++ core/server.js
@@ -9,20 +9,22 @@ const querystring = require('querystring');
 
 function onRequest(req, res) {
     var query_string = querystring.parse((req.url).substring(1));
-    res.writeHead(200, { "Content-Type": "application/json" });
     if(query_string.listtablecontent != undefined) {
       db.listTable(req, res);
     }
     else if(query_string.firstname != undefined) {
-      res.end("Successfully added content. firstname: " + query_string.firstname + ", lastname: " + query_string.lastname);
-      mqtt_client.publish('test', 'added'+JSON.stringify(query_string));
-      db.addValue(req, res, [query_string.firstname, query_string.lastname]);
+      mqtt_client.publish('xagora', 'added'+JSON.stringify(query_string));
+      db.addValue(req, res, query_string);
+    }
+    else if(query_string.end != undefined) {
+      res.end("ending");
+      db.endPool();
+      mqtt_client.end();
     }
     else {
-      res.end("Oops looks like this is not a valid url for this Node web API, try adding a name with /firstname=___&lastname=______, or see your current table of names in JSON format with /listtablecontent");
+      res.end("Oops looks like this is not a valid url for this Node web API, if you are using node-red try adding a name with /add?firstname=___&lastname=______, or see your current table of names in JSON format with /noderedlist. If you are not going through node-red try adding a name with /firstname=___&lastname=______, or see your current table of names in JSON format with /listtablecontent");
     }
 }
 
 http.createServer(onRequest).listen(port);
-mqtt_client.end();
 console.log("Server has started.");
